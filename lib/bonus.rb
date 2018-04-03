f5111cfa9121736a1295e66457c82ee098acf416
Chris Dundon <chris.dundon@gmail.com>
2018-04-03 16:56:32 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,7 +24,7 @@ def bonus
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-  puts epic_tragedy[:montague][:hero][:statuss] 
+  puts epic_tragedy[:montague][:hero][:status] 
   puts epic_tragedy[:capulet][:heroine][:status]
   
 
